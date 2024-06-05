e5b6a7ee55a56a54fc60df9d255912889527f5b0
Liang Huang <sammy.huang@zilliz.com>
2024-06-05 22:23:02 +0800

    .
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- ci/e2e.groovy
+++ ci/e2e.groovy
@@ -44,14 +44,14 @@ pipeline {
                 container('main') {
                     script {
                         // checkout scm
+                        // git submodule update --init --recursive
 
+                            // chown -R jenkins:jenkins /home/jenkins/agent/workspace/thirdparty/
+                            // git config --global --add safe.directory /home/jenkins/agent/workspace
+                            // git config --global --add safe.directory /home/jenkins/agent/workspace/thirdparty
+                            // git config --global --add safe.directory /home/jenkins/agent/workspace/thirdparty/milvus
                         sh '''
-                        git submodule update --init --recursive
 
-                            chown -R jenkins:jenkins /home/jenkins/agent/workspace/thirdparty/
-                            git config --global --add safe.directory /home/jenkins/agent/workspace
-                            git config --global --add safe.directory /home/jenkins/agent/workspace/thirdparty
-                            git config --global --add safe.directory /home/jenkins/agent/workspace/thirdparty/milvus
 
                         MIRROR_URL="https://docker-nexus-ci.zilliz.cc" ./ci/set_docker_mirror.sh
                         '''

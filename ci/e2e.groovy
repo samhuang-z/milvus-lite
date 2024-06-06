64a78832b04d9deef4e178926f1732560f8bbe55
Liang Huang <sammy.huang@zilliz.com>
2024-06-06 11:10:51 +0800

    .
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- ci/e2e.groovy
+++ ci/e2e.groovy
@@ -25,17 +25,17 @@ pipeline {
             }
     }
     environment {
-        PROJECT_NAME = 'milvus'
-        SEMVER = "${BRANCH_NAME.contains('/') ? BRANCH_NAME.substring(BRANCH_NAME.lastIndexOf('/') + 1) : BRANCH_NAME}"
         DOCKER_BUILDKIT = 1
-        ARTIFACTS = "${env.WORKSPACE}/_artifacts"
-        CI_DOCKER_CREDENTIAL_ID = 'harbor-milvus-io-registry'
-        MILVUS_HELM_NAMESPACE = 'milvus-ci'
-        DISABLE_KIND = true
-        HUB = 'harbor.milvus.io/milvus'
-        JENKINS_BUILD_ID = "${env.BUILD_ID}"
-        CI_MODE = 'pr'
-        SHOW_MILVUS_CONFIGMAP = true
+        // PROJECT_NAME = 'milvus'
+        // SEMVER = "${BRANCH_NAME.contains('/') ? BRANCH_NAME.substring(BRANCH_NAME.lastIndexOf('/') + 1) : BRANCH_NAME}"
+        // ARTIFACTS = "${env.WORKSPACE}/_artifacts"
+        // CI_DOCKER_CREDENTIAL_ID = 'harbor-milvus-io-registry'
+        // MILVUS_HELM_NAMESPACE = 'milvus-ci'
+        // DISABLE_KIND = true
+        // HUB = 'harbor.milvus.io/milvus'
+        // JENKINS_BUILD_ID = "${env.BUILD_ID}"
+        // CI_MODE = 'pr'
+        // SHOW_MILVUS_CONFIGMAP = true
     }
 
     stages {
@@ -43,30 +43,15 @@ pipeline {
             steps {
                 container('main') {
                     script {
-                        // checkout scm
-                        // git submodule update --init --recursive
-
-                        // chown -R jenkins:jenkins /home/jenkins/agent/workspace/thirdparty/
-                        // git config --global --add safe.directory /home/jenkins/agent/workspace
-                        // git config --global --add safe.directory /home/jenkins/agent/workspace/thirdparty
-                        // git config --global --add safe.directory /home/jenkins/agent/workspace/thirdparty/milvus
                         sh '''
 
                         MIRROR_URL="https://docker-nexus-ci.zilliz.cc" ./ci/set_docker_mirror.sh
                         '''
-                        // sh 'printenv'
-                        // def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
                         sh '''
                          docker run --net=host  \
                             -e CONAN_USER_HOME=/root/  -v \$PWD:/root/milvus-lite -v /root/.conan:/root/.conan -w /root/milvus-lite  \
                             milvusdb/milvus-env:ubuntu20.04-20240520-d27db99 bash ci/entrypoint.sh
                          '''
-                        // dir('scripts') {
-                        //     sh 'chmod +x ./build.sh'
-                        //     sh './build.sh main /root'
-                        // }
-
-                    // sh 'sleep 600'
                     }
                 }
             }
@@ -94,11 +79,6 @@ pipeline {
         stage('Test') {
             steps {
                 container('pytest') {
-                        // sh '''
-                        //
-                        // pip install ./python/dist/*.whl
-                        //
-                        // '''
 
                             sh '''
                               bash ci/test.sh
